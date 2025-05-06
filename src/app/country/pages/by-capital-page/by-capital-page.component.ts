34c37ce58545a643ba09ce46c80057903c6221f7
Enoc Castro Vargas <enocco70@gmail.com>
2025-05-06 17:43:38 -0600

    feat: Creando el CountryService + interfaces

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { CountryService } from '../../services/country.service';
+
 import { SearchInputComponent } from '../../components/search-input/search-input.component';
-import { CountryListComponent } from "../../components/country-list/country-list.component";
+import { CountryListComponent } from '../../components/country-list/country-list.component';
 
 @Component({
   selector: 'country-by-capital-page',
@@ -8,7 +11,13 @@ import { CountryListComponent } from "../../components/country-list/country-list
   templateUrl: './by-capital-page.component.html',
 })
 export class ByCapitalPageComponent {
-  onSearch(value: string) {
-    console.log({ value });
+  countryService = inject(CountryService);
+
+  onSearch(query: string) {
+    if (query =='') return;
+
+    this.countryService.searchByCapital(query).subscribe((data) => {
+      console.log(data);
+    });
   }
 }
