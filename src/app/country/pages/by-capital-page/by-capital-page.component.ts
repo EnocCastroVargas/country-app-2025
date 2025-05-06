dc05221f5c3ea133f7f02220afd201d4c40523c1
Enoc Castro Vargas <enocco70@gmail.com>
2025-05-06 17:58:35 -0600

    feat: Se crea el country interface y el mapeo de datos

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 
 import { CountryService } from '../../services/country.service';
 
 import { SearchInputComponent } from '../../components/search-input/search-input.component';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
+import { RESTCountry } from '../../interfaces/rest-countries.interface';
 
 @Component({
   selector: 'country-by-capital-page',
@@ -13,11 +14,23 @@ import { CountryListComponent } from '../../components/country-list/country-list
 export class ByCapitalPageComponent {
   countryService = inject(CountryService);
 
-  onSearch(query: string) {
-    if (query =='') return;
+  isLoading = signal(false);
+  isError = signal<string | null>(null);
+  countries = signal<RESTCountry[]>([]);
 
-    this.countryService.searchByCapital(query).subscribe((data) => {
-      console.log(data);
+  onSearch(query: string) {
+    if (query == '') return;
+
+    if (this.isLoading()) return;
+
+    this.isLoading.set(true);
+    this.isError.set(null);
+
+    this.countryService.searchByCapital(query).subscribe((countries) => {
+      this.isLoading.set(false);
+      this.countries.set(countries);
+
+      console.log(countries);
     });
   }
 }
